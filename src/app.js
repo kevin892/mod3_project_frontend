0da65eaf2c7f68659824019e1136f0c0c7b0e39a
Kevin <kevinbruggiero@gmail.com>
2019-05-06 20:30:15 -0600

    add flashcard style

--- src/app.js
+++ src/app.js
@@ -1,39 +1,40 @@
+/*jshint esversion: 6 */
+
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
 
   const endPoint = 'http://localhost:3000/api/v1/lessons';
   const container = document.querySelector(".container");
-  const lessonForm = document.querySelector(".lesson-form")
-  lessonForm.style.display = "none"
+  const lessonForm = document.querySelector(".lesson-form");
+  lessonForm.style.display = "none";
 
 
   function createElement(element) {
-    return document.createElement(element)
+    return document.createElement(element);
   }
 
   function append(parent, element) {
-    return parent.appendChild(element)
+    return parent.appendChild(element);
   }
 
   function addClass(element, className) {
-    return element.classList.add(className)
+    return element.classList.add(className);
   }
 
 
-     const toggleButton = document.querySelector(".toggle-button")
+     const toggleButton = document.querySelector(".toggle-button");
 
      toggleButton.addEventListener("click", function() {
        if(lessonForm.style.display == '' || lessonForm.style.display == "block")
           lessonForm.style.display = 'none';
        else
           lessonForm.style.display = 'block';
-     })
+     });
 
 
 
 
   function makeLesson(lesson) {
-
     const lessonDiv = createElement("div"),
      lessonName = createElement("h2"),
      lessonInstructor = createElement("h5"),
@@ -43,41 +44,87 @@ document.addEventListener('DOMContentLoaded', (event) => {
      lessonCode = createElement("a"),
      codeIcon = createElement("i"),
      lessonLikes = createElement("p");
-     deleteButton = createElement("button")
+     deleteButton = createElement("i")
+     likeButton = createElement("i")
+     contentDiv = createElement("div")
 
      lessonName.innerHTML = lesson.name;
 
-     lessonInstructor.innerHTML = lesson.instructor;
+     lessonInstructor.innerHTML = "Instructor: " + lesson.instructor;
 
      if (lesson.mod_id) {
-       lessonMod.innerHTML = lesson.mod_id
+       lessonMod.innerHTML = "Module: " + lesson.mod_id
      }
      else {
-       lessonMod.innerHTML = lesson.mod.name
+       lessonMod.innerHTML = "Module: " + lesson.mod.name
      }
-
+     if (lesson.video) {
      lessonVideo.href = "http://" + lesson.video;
+   } else {
+     lessonVideo.style.display = "none"
+   }
+
+   if (lesson.code) {
+   lessonCode.href = "http://" + lesson.code;
+ } else {
+   lessonCode.style.display = "none"
+ }
      lessonCode.href = "http://" + lesson.code;
      lessonLikes.innerHTML = lesson.likes;
-     deleteButton.innerHTML = "Delete"
-     deleteButton.classList.add(lesson.id)
+     deleteButton.id = lesson.id
+     deleteButton.classList.add("fa-trash-alt")
+     deleteButton.classList.add("fas")
+     deleteButton.classList.add("delete-button")
+     deleteButton.classList.add("hvr-grow")
+
+     codeIcon.classList.add("fa-github-square")
+     codeIcon.classList.add("fab")
+     codeIcon.classList.add("code-icon")
+     codeIcon.classList.add("hvr-shrink")
+
+     videoIcon.classList.add("fa-youtube")
+     videoIcon.classList.add("fab")
+     videoIcon.classList.add("video-icon")
+     videoIcon.classList.add("hvr-shrink")
+
+     likeButton.classList.add("fas")
+     likeButton.classList.add("fa-thumbs-up")
+     likeButton.classList.add("like-icon")
+     likeButton.classList.add("hvr-grow")
+    
+
+     contentDiv.classList.add("content-div")
+
      lessonDiv.classList.add("col-sm")
      lessonDiv.classList.add("hvr-grow")
 
+     lessonLikes.classList.add("lesson-likes")
+
 
      deleteButton.addEventListener("click", function(event) {
-       let lessonId = parseInt(this.classList[0])
+       let lessonId = parseInt(this.id)
+
        this.parentElement.remove()
        deleteLesson(lessonId)
      })
 
+     likeButton.addEventListener("click", function(event) {
+       this.classList.add("animate")
+       this.classList.add("bounceOut")
+    console.log("Clicked!");
+     })
+
      append(lessonDiv, lessonName);
-     append(lessonDiv, lessonInstructor);
-     append(lessonDiv, lessonMod);
+     append(contentDiv, lessonInstructor);
+     append(contentDiv, lessonMod);
      append(lessonDiv, lessonVideo);
      append(lessonDiv, lessonCode);
      append(lessonDiv, lessonLikes);
      append(lessonDiv, deleteButton)
+     append(lessonCode, codeIcon)
+     append(lessonVideo, videoIcon)
+     append(lessonDiv, likeButton)
+     append(lessonDiv, contentDiv)
      append(container, lessonDiv);
   }
 
@@ -105,16 +152,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         instructor: instructor,
         mod_id: parseInt(mod),
         likes: 0,
-      }
+      };
 
-      // document.write(`${data.name} was added!`)
-
-      // function loadPage(){
-      //   return location.reload();
-      // }
       makeLesson(data)
-      // window.setTimeout(loadPage, 2000)
           this.reset()
+          lessonForm.style.display = "none"
     fetch(endPoint, {
         method: "POST",
         body: JSON.stringify(data),
@@ -124,7 +166,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
       })
       .catch(error => console.log(error))
-  })
+  });
 
 
 function deleteLesson(lessonId) {
@@ -133,4 +175,4 @@ function deleteLesson(lessonId) {
   })
   .catch(error => console.log(error))
 }
-})
+});
